f6cb67c3507e7ffa770257dfbee2b5bd3a902f6d
Ott Tooming <OttTooming@gmail.com>
2018-03-15 14:46:11 +0200

    Add delegate function List

--- eek_2018_oop_6/Program.cs
+++ eek_2018_oop_6/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace eek_2018_oop_6
 {
-    class MainClass
+    public delegate void tootmisprotsess(); // delegatsiooni deklareerimine
+
+    class Program
     {
-        public static void Main(string[] args)
+        static List<tootmisprotsess> tootmisprotsessid = new List<tootmisprotsess>();
+
+        public static void FR() { Console.WriteLine("Teostamisel on freesimine"); }
+        public static void LI() { Console.WriteLine("Teostamisel on lihvimine"); }
+        public static void VA() { Console.WriteLine("Teostamisel on varvimine"); }
+        public static void KK() { Console.WriteLine("Kvaliteedi kontroll"); }
+        public static void TE() { Console.WriteLine("Testimine"); }
+
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create delegation function object
+            tootmisprotsess prFR = new tootmisprotsess(FR);
+            tootmisprotsess prLI = new tootmisprotsess(LI);
+            tootmisprotsess prVA = new tootmisprotsess(VA);
+            tootmisprotsess prKK = new tootmisprotsess(KK);
+            tootmisprotsess prTE = new tootmisprotsess(TE);
+
+            // Lisa delegaatfuntisioonide objektiivid massiivi
+            // mida meil on vooo1imalik proge too kaigus muuta
+            tootmisprotsessid.Add(prFR);
+            tootmisprotsessid.Add(prLI);
+            tootmisprotsessid.Add(prVA);
+            tootmisprotsessid.Add(prKK);
+            tootmisprotsessid.Add(prTE);
+
+            foreach(tootmisprotsess x in tootmisprotsessid)
+            {
+                x();
+            }
+
+            Console.Read();
         }
     }
 }
